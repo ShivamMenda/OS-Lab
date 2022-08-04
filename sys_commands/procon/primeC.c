4c6ea0adc00d33596a6cf2073c938cfcd8c7148f
Shivam Menda <shivammenda2002@gmail.com>
2022-08-04 23:03:14 +0530

    Prime Shared Memory program updated.

--- sys_commands/procon/primeC.c
+++ sys_commands/procon/primeC.c
@@ -1,42 +1,46 @@
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
-#include <string.h>
-#include<sys/ipc.h>
-#include <unistd.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<sys/mman.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<fcntl.h>
+
 int main(int argc,char *argv[])
 {
-         int k,n1,n2,n3,flag=0,f;
-         void *ptr;
-         int shm_fd = shm_open("OS", O_CREAT | O_RDWR, 0666);
-         ftruncate(shm_fd,4096);
-         ptr = mmap(0,4096,PROT_WRITE, MAP_SHARED, shm_fd, 0);
-         printf("CHILD:\n");
-         int i=atoi(argv[1]);
-         int j=atoi(argv[2]);
-         for(k=i+1;k<=j;k++)
-         {
-                flag=0;
-                for(j=2; j<=i/2; j++)
-                {
-                    if(i%j==0)
-                    {
-                        flag=1;
-                        break;
-                    }
-                }
-                if(flag==0)
-                {
-                    printf("%d ",i);
-                   sprintf(ptr,"%d ",i);
-                    ptr += strlen(ptr); 
-                } 
-                      
-	    }
+ void *ptr;
+ int shm_fd=shm_open("os",O_CREAT|O_RDWR,0666);
+ ftruncate(shm_fd,4096);
+ ptr=mmap(0,4096,PROT_WRITE,MAP_SHARED,shm_fd,0);
+ printf("child\n");
+ int i=atoi(argv[1]);
+ int j=atoi(argv[2]);
+ if(i==1 || i==2)
+ {
+  sprintf(ptr,"2  ");
+  printf("2  ");
+  ptr+=strlen(ptr);
+  i++;
+  if(i==2)
+   i++;
+ }
+ for(int m=i;m<=j;m++)
+ {
+  int flag=0;
+  for(int n=2;n<m;n++)
+   if(m%n==0)
+    flag=1;
+  if(flag==0)
+  {
+   sprintf(ptr,"%d  ",m);
+   printf("%d  ",m);
+   ptr+=strlen(ptr);
+  }
+ } 
+ 
 }
-
--- sys_commands/procon/primeP.c
+++ sys_commands/procon/primeP.c
@@ -1,59 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
 #include<string.h>
+#include<unistd.h>
 #include<fcntl.h>
-#include<sys/mman.h>
-#include<sys/stat.h>
+#include<sys/types.h>
 #include<sys/wait.h>
+#include<sys/shm.h>
+#include<sys/stat.h>
+#include<sys/mman.h>
 
 int main(int argc,char *argv[])
 {
-    int i,j;
-    pid_t pid;
-    int k;
-    int n1,n2,n3;
-    const int SIZE = 4096;
-        int shm_fd;
-    void *ptr;
-    if (argc > 2)
-    {
-        sscanf(argv[1], "%d", &i);
-        sscanf(argv[2], "%d", &j);
-        
-        if (i < 1)
-        {
-            printf("Error input: %d\n", i);
-            return 0;
-        }
-        if (j < 1)
-        {
-            printf("Error input: %d\n", j);
-            return 0;
-        }
-    }
-    else
-    {
-      printf("Error: not passing N in command line\n");
-      exit(0);
-    }
-    pid = fork();
-    if (pid == 0)// CHILD
-    {   
-        execlp("./prime","prime",argv[1],argv[2],NULL);
-    }
-    else if (pid > 0)
-    {
-        wait(NULL);
-        printf("\nPARENT: child completed\n");
-        shm_fd = shm_open("OS", O_RDONLY, 0666);
-        ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
-        printf("Parent printing:\n");
-        printf("%s ", (char *)ptr);
-        shm_unlink("OS");
-    }
-    return 0;
+ pid_t pid;
+ int i;
+ int shm_fd;
+ void *ptr;
+ if(argc>2)
+ {
+  sscanf(argv[1],"%d",&i);
+  if(i<1)
+  {
+   printf("error\n");
+   return 0;
+  }
+ }
+ else
+ {
+  printf("not passing n\n");
+  exit(0);
+ }
+ pid=fork();
+ if(pid==0)
+ {
+  execlp("./prime","prime",argv[1],argv[2],NULL);
+ } 
+ else
+ {
+  printf("parent\n");
+  wait(NULL);
+  printf("child completed\n");
+  shm_fd=shm_open("os",O_RDONLY,0666);
+  ptr=mmap(0,4096,PROT_READ,MAP_SHARED,shm_fd,0);
+  printf("parent printing\n");
+  printf("%s",(char *)ptr);
+  shm_unlink("os");
+ }
+ return 0;
 }
